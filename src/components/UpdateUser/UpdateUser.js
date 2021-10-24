f2bc3c306409ae231ed00fec51666b6c03717b0c
Rohul Amin <rohulamin83716@gmail.com>
2021-10-25 00:50:04 +0600

    added updated user and email

--- src/components/UpdateUser/UpdateUser.js
+++ src/components/UpdateUser/UpdateUser.js
@@ -10,10 +10,52 @@ const UpdateUser = () => {
       .then((res) => res.json())
       .then((data) => sertUser(data));
   }, []);
+  const hanldeNameChange = (e) => {
+    const updateName = e.target.value;
+    const updateUser = { name: updateName, email: user.email };
+    sertUser(updateUser);
+  };
+  const handleEmailChange = (e) => {
+    const updateEmail = e.target.value;
+    const updateUser = { email: updateEmail, name: user.name };
+    sertUser(updateUser);
+  };
+  const hanldeUpdate = (e) => {
+    const url = `http://localhost:5000/users/${id}`;
+    fetch(url, {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.modifiedCount > 0) {
+          alert("Updated data successfully!");
+
+          sertUser({});
+        }
+      });
+    e.preventDefault();
+  };
   return (
     <div>
       <h2> Update {user.name}</h2>
       <small>{id}</small>
+      <form onClick={hanldeUpdate}>
+        <input
+          type="text"
+          value={user.name || ""}
+          onChange={hanldeNameChange}
+        />
+        <input
+          type="email"
+          value={user.email || ""}
+          onChange={handleEmailChange}
+        />
+        <input type="submit" value="Update" />
+      </form>
     </div>
   );
 };
